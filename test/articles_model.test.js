0f74298e51b3a0d8fd43a511c50ef6201ce7d357
Santiago <santidiazcurutchet@gmail.comsantidiazcurutchet@gmail.comgit config --global user.email santidiazcurutchet@gmail.com>
2020-05-26 12:04:50 -0300

    libraries updated

--- test/articles_model.test.js
+++ test/articles_model.test.js
@@ -14,12 +14,12 @@ const db = require("../models/database");
  *
  */
 
-describe("El modelo `Article`", function() {
+describe("El modelo `Article`", function () {
   /**
    * Primero limpiamos la base de datos y recreamos las tablas antes de empezar
    */
-  before(function() {
-    return db.sync({ force: true });
+  before(function (done) {
+    db.sync({ force: true }).then(() => done());
   });
 
   /**
@@ -29,71 +29,71 @@ describe("El modelo `Article`", function() {
     "The South African cliff swallow (Petrochelidon spilodera), also known as the South African swallow, is a species of bird in the Hirundinidae family.";
 
   let article;
-  beforeEach(function() {
+  beforeEach(function () {
     article = Article.build({
       title: "Migratory Birds",
-      content: fullText
+      content: fullText,
     });
   });
 
   /**
    * Ademas, vaciamos las tablas luego de cada spec
    */
-  afterEach(function() {
-    return Promise.all([
+  afterEach(function (done) {
+    Promise.all([
       Article.truncate({ cascade: true }),
-      User.truncate({ cascade: true })
-    ]);
+      User.truncate({ cascade: true }),
+    ]).then(() => done());
   });
 
-  describe("definción de atributos", function() {
+  describe("definción de atributos", function () {
     /**
      * Tu modelo debería tener dos campos (ambos requeridos): `title` y `content`.
      *
      * http://docs.sequelizejs.com/manual/tutorial/models-definition.html
      */
-    it("incluye los campos `title` y `content`", function() {
-      return article.save().then(function(savedArticle) {
+    it("incluye los campos `title` y `content`", function () {
+      return article.save().then(function (savedArticle) {
         expect(savedArticle.title).to.equal("Migratory Birds");
         expect(savedArticle.content).to.equal(fullText);
       });
     });
 
-    xit("requiere `content`", function() {
+    it("requiere `content`", function () {
       article.content = null;
 
       return article.validate().then(
-        function() {
+        function () {
           throw new Error("la validación debe fallar cuando content es null");
         },
-        function(result) {
+        function (result) {
           expect(result).to.be.an.instanceOf(Error);
         }
       );
     });
 
-    xit("requiere `title` (en una forma más estricta que para `content)", function() {
+    it("requiere `title` (en una forma más estricta que para `content)", function () {
       article.title = "";
 
       return article.validate().then(
-        function() {
+        function () {
           throw new Error("La validación debe fallar cuando title esta vacío");
         },
-        function(result) {
+        function (result) {
           expect(result).to.be.an.instanceOf(Error);
           expect(result.message).to.contain("Validation error");
         }
       );
     });
 
-    xit("puede manejar `content` extenso", function() {
+    it("puede manejar `content` extenso", function () {
       const articleContent =
         "WALL-E (stylized with an interpunct as WALL·E) is a 2008 American computer-animated science-fiction comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. Directed by Andrew Stanton, the story follows a robot named WALL-E, who is designed to clean up an abandoned, waste-covered Earth far in the future. He falls in love with another robot named EVE, who also has a programmed task, and follows her into outer space on an adventure that changes the destiny of both his kind and humanity. Both robots exhibit an appearance of free will and emotions similar to humans, which develop further as the film progresses.";
 
       return Article.create({
         title: "WALL-E",
-        content: articleContent
-      }).then(function(result) {
+        content: articleContent,
+      }).then(function (result) {
         expect(result).to.be.an("object");
         expect(result.title).to.equal("WALL-E");
         expect(result.content).to.equal(articleContent);
@@ -110,8 +110,8 @@ describe("El modelo `Article`", function() {
    * Article en tu código, las Routes también van a fallar. Hace commits!
    */
 
-  describe("definición de opciones", function() {
-    describe("`snippet` campo virtual", function() {
+  describe("definición de opciones", function () {
+    describe("`snippet` campo virtual", function () {
       /**
        * Configurá un campo virtual (Fijate en los getter methods de
        * sequelize) llamado `snippet` que devuelva los primeros 23
@@ -119,7 +119,7 @@ describe("El modelo `Article`", function() {
        *
        * http://docs.sequelizejs.com/manual/tutorial/models-definition.html#defining-as-part-of-the-model-options
        */
-      xit('Evalua a los primeros 23 caracteres del `content` anexado con "..."', function() {
+      it('Evalua a los primeros 23 caracteres del `content` anexado con "..."', function () {
         expect(article.snippet).to.equal("The South African cliff...");
 
         article.content =
@@ -132,14 +132,14 @@ describe("El modelo `Article`", function() {
       });
 
       // Esto es principalmente para evitar un caso limite visto durante `Model.update`
-      xit("retorna un empty string para contenido faltante", function() {
+      it("retorna un empty string para contenido faltante", function () {
         article.content = undefined;
 
         expect(article.snippet).to.equal("");
       });
     });
 
-    describe("metodo de instancia: `truncate`", function() {
+    describe("metodo de instancia: `truncate`", function () {
       /**
        * Configurá un metodo de instancia (Fijate los instanceMethods de
        * sequelize) llamado `truncate` que va a acortar (cambiar!) el
@@ -150,14 +150,14 @@ describe("El modelo `Article`", function() {
        *
        * http://docs.sequelizejs.com/manual/tutorial/models-definition.html#expansion-of-models
        */
-      xit("trunca el `content`", function() {
+      it("trunca el `content`", function () {
         expect(article.content).to.equal(fullText);
 
         article.truncate(12);
         expect(article.content).to.equal("The South Af");
       });
 
-      xit("acepta cualquier length", function() {
+      it("acepta cualquier length", function () {
         expect(article.content).to.equal(fullText);
 
         const randLength = Math.ceil(Math.random() * 20);
@@ -165,19 +165,19 @@ describe("El modelo `Article`", function() {
         expect(article.content).to.have.length(randLength);
       });
 
-      xit("No guarda la instancia una vez truncada", function() {
+      it("No guarda la instancia una vez truncada", function () {
         expect(article.content).to.equal(fullText);
 
         article.truncate(7);
         expect(article.content).to.have.length(7);
 
-        return Article.findAll().then(function(articles) {
+        Article.findAll().then(function (articles) {
           expect(articles).to.have.length(0);
         });
       });
     });
 
-    describe("Metodo de Clase: `findByTitle`", function() {
+    describe("Metodo de Clase: `findByTitle`", function () {
       /**
        * Seteá un metodo de clase llamado findByTitle que nos va a servir
        * para encontar *un solo* documento por su titulo.
@@ -185,19 +185,19 @@ describe("El modelo `Article`", function() {
        * http://docs.sequelizejs.com/manual/tutorial/models-definition.html#expansion-of-models
        */
 
-      beforeEach(function() {
-        const otherArticles = [1, 2, 3].map(function(num) {
+      beforeEach(function () {
+        const otherArticles = [1, 2, 3].map(function (num) {
           return Article.create({
             title: "Article Number " + num,
-            content: "etc."
+            content: "etc.",
           });
         });
         const articles = otherArticles.concat(article.save());
         return Promise.all(articles);
       });
 
-      xit("Encuentra un artículo especifico por su `title`", function() {
-        return Article.findByTitle("Migratory Birds").then(function(
+      it("Encuentra un artículo especifico por su `title`", function () {
+        return Article.findByTitle("Migratory Birds").then(function (
           foundArticle
         ) {
           expect(foundArticle).not.to.be.an.instanceOf(Array);
@@ -207,7 +207,7 @@ describe("El modelo `Article`", function() {
     });
   });
 
-  describe("asociaciones", function() {
+  describe("asociaciones", function () {
     /**
      * Agregá una relación `belongsTo` entre articulos y users, pero
      * asegurate que el usuario tiene el alias de 'author' para cada
@@ -216,26 +216,26 @@ describe("El modelo `Article`", function() {
      * http://docs.sequelizejs.com/manual/tutorial/associations.html#belongsto
      */
 
-    xit("pertenece a un user, que es guardado como el `author` del articulo", function() {
+    it("pertenece a un user, que es guardado como el `author` del articulo", function () {
       const creatingUser = User.create({ name: "Alatar the Blue" });
       const creatingArticle = Article.create({
         title: "Blue Wizards",
         content:
-          "They are two of the five Wizards (or Istari) sent by the Valar to Middle-earth to aid in the struggle against Sauron."
+          "They are two of the five Wizards (or Istari) sent by the Valar to Middle-earth to aid in the struggle against Sauron.",
       });
 
       return Promise.all([creatingUser, creatingArticle])
-        .spread(function(createdUser, createdArticle) {
+        .spread(function (createdUser, createdArticle) {
           // este metodo `setAuthor` existe automaticamente si seteas la asociación correctamente
           return createdArticle.setAuthor(createdUser);
         })
-        .then(function() {
+        .then(function () {
           return Article.findOne({
             where: { title: "Blue Wizards" },
-            include: { model: User, as: "author" }
+            include: { model: User, as: "author" },
           });
         })
-        .then(function(foundArticle) {
+        .then(function (foundArticle) {
           expect(foundArticle.author).to.exist; // eslint-disable-line no-unused-expressions
           expect(foundArticle.author.name).to.equal("Alatar the Blue");
         });
@@ -249,51 +249,51 @@ describe("El modelo `Article`", function() {
    * http://docs.sequelizejs.com/manual/tutorial/hooks.html
    */
 
-  describe("campo `version`", function() {
-    beforeEach(function() {
+  describe("campo `version`", function () {
+    beforeEach(function () {
       return Article.create({
         title: "Biological Immortality",
         content:
-          "Biological immortality refers to a stable or decreasing rate of mortality from senescence, thus decoupling it from chronological age."
+          "Biological immortality refers to a stable or decreasing rate of mortality from senescence, thus decoupling it from chronological age.",
       });
     });
 
-    xit("es originalmente 0, incluso si no esta explicitamente seteado", function() {
+    it("es originalmente 0, incluso si no esta explicitamente seteado", function () {
       return Article.findOne({
-        where: { title: "Biological Immortality" }
-      }).then(function(foundArticle) {
+        where: { title: "Biological Immortality" },
+      }).then(function (foundArticle) {
         expect(foundArticle.version).to.equal(0);
       });
     });
 
-    xit("incrementa por 1 cada vez que el articulo se actualiza", function() {
+    it("incrementa por 1 cada vez que el articulo se actualiza", function () {
       return Article.findOne({ where: { title: "Biological Immortality" } })
-        .then(function(foundArticle) {
+        .then(function (foundArticle) {
           expect(foundArticle.version).to.equal(0);
           return foundArticle.update({
-            content: "Biological immortality is a lie!"
+            content: "Biological immortality is a lie!",
           });
         })
-        .then(function(updatedArticle) {
+        .then(function (updatedArticle) {
           expect(updatedArticle.version).to.equal(1);
           return updatedArticle.update({
-            content: "Have you seen the 19th century painting of Keanu Reeves?"
+            content: "Have you seen the 19th century painting of Keanu Reeves?",
           });
         })
-        .then(function(updatedArticle) {
+        .then(function (updatedArticle) {
           expect(updatedArticle.version).to.equal(2);
 
           // "recarga" el articulo de la base de datos solo para asegurarse
           // que los cambios a la versión son guardados apropiadamente!
           return updatedArticle.reload();
         })
-        .then(function(reloadedArticle) {
+        .then(function (reloadedArticle) {
           expect(reloadedArticle.version).to.equal(2);
         });
     });
   });
 
-  describe("extra credito campo `tags`", function() {
+  describe("extra credito campo `tags`", function () {
     /** EXTRA CREDITO
      * Tu modelo Article debería tener un campo tag que es
      * un arreglo, pero cuando lo accedemos, deberíamos tener
@@ -304,7 +304,7 @@ describe("El modelo `Article`", function() {
      *
      * Para activar este spec, cambia `xit` a `it`
      */
-    xit("es un getter customizado", function() {
+    it("es un getter customizado", function () {
       // tags deberían tener un `defaultValue` que es un arreglo vacío.
       // console.dir(Article);
       expect(Article.tableAttributes.tags.defaultValue).to.deep.equal([]);
@@ -313,8 +313,8 @@ describe("El modelo `Article`", function() {
       return Article.create({
         title: "Taggy",
         content: "So Taggy",
-        tags: ["tag1", "tag2", "tag3"]
-      }).then(function(createdArticle) {
+        tags: ["tag1", "tag2", "tag3"],
+      }).then(function (createdArticle) {
         expect(createdArticle.tags).to.equal("tag1, tag2, tag3");
       });
     });
--- test/articles_route.test.js
+++ test/articles_route.test.js
@@ -17,25 +17,25 @@ const User = require("../models/user");
  * Hace esto después de terminar los primeros test del Modelo de Article
  *
  */
-describe("Articles Route:", function() {
+describe("Articles Route:", function () {
   /**
    * Primero limpiamos la base de datos antes de comenzar
    */
-  before(function() {
+  before(function () {
     return db.sync({ force: true });
   });
 
   /**
    * También, vaciamos las tablas luego de cada spec
    */
-  afterEach(function() {
+  afterEach(function () {
     return Promise.all([
       Article.truncate({ cascade: true }),
-      User.truncate({ cascade: true })
+      User.truncate({ cascade: true }),
     ]);
   });
 
-  describe("GET /articles", function() {
+  describe("GET /articles", function () {
     /**
      * Problema 1
      * Haremos un request GET a /articles
@@ -45,12 +45,12 @@ describe("Articles Route:", function() {
      *
      * **Credito Extra**: Considera usando app.param para automaticamente agregar el Article donde haya un param :id detectado
      */
-    xit("responde con un array via JSON", function() {
+    it("responde con un array via JSON", function () {
       return agent
         .get("/articles")
         .expect("Content-Type", /json/)
         .expect(200)
-        .expect(function(res) {
+        .expect(function (res) {
           // res.body es el objeto JSON retornado
           expect(res.body).to.be.an.instanceOf(Array);
           expect(res.body).to.have.length(0);
@@ -63,17 +63,17 @@ describe("Articles Route:", function() {
      * luego retornalo usando la ruta GET /articles
      *
      */
-    xit("retorna un articulo si hay uno en la base de datos", function() {
+    it("retorna un articulo si hay uno en la base de datos", function () {
       var article = Article.build({
         title: "Test Article",
-        content: "Test body"
+        content: "Test body",
       });
 
-      return article.save().then(function() {
+      return article.save().then(function () {
         return agent
           .get("/articles")
           .expect(200)
-          .expect(function(res) {
+          .expect(function (res) {
             expect(res.body).to.be.an.instanceOf(Array);
             expect(res.body[0].content).to.equal("Test body");
           });
@@ -86,27 +86,27 @@ describe("Articles Route:", function() {
      * modelo, luego retornalo usando la ruta GET /articles
      *
      */
-    xit("retorna otro articulo si hay uno en la base de datos", function() {
+    it("retorna otro articulo si hay uno en la base de datos", function () {
       const article1 = Article.build({
         title: "Test Article",
-        content: "Test body"
+        content: "Test body",
       });
 
       const article2 = Article.build({
         title: "Another Test Article",
-        content: "Another test body"
+        content: "Another test body",
       });
 
       return article1
         .save()
-        .then(function() {
+        .then(function () {
           return article2.save();
         })
-        .then(function() {
+        .then(function () {
           return agent
             .get("/articles")
             .expect(200)
-            .expect(function(res) {
+            .expect(function (res) {
               expect(res.body).to.be.an.instanceOf(Array);
               expect(res.body[0].content).to.equal("Test body");
               expect(res.body[1].content).to.equal("Another test body");
@@ -118,26 +118,26 @@ describe("Articles Route:", function() {
   /**
    * Busca un articulo por su id
    */
-  describe("GET /articles/:id", function() {
+  describe("GET /articles/:id", function () {
     let coolArticle;
 
-    beforeEach(function() {
+    beforeEach(function () {
       const creatingArticles = [
         {
           title: "Boring article",
-          content: "This article is boring"
+          content: "This article is boring",
         },
         {
           title: "Cool Article",
-          content: "This article is cool"
+          content: "This article is cool",
         },
         {
           title: "Riveting Article",
-          content: "This article is riveting"
-        }
-      ].map(data => Article.create(data));
+          content: "This article is riveting",
+        },
+      ].map((data) => Article.create(data));
 
-      return Promise.all(creatingArticles).then(createdArticles => {
+      return Promise.all(creatingArticles).then((createdArticles) => {
         coolArticle = createdArticles[1];
       });
     });
@@ -146,11 +146,11 @@ describe("Articles Route:", function() {
      * Este es un correcto pedido a GET /articles/:id donde buscamos
      * por el id del articulo creado arriba.
      */
-    xit("retorna el JSON del articulo basado en el id", function() {
+    it("retorna el JSON del articulo basado en el id", function () {
       return agent
         .get("/articles/" + coolArticle.id)
         .expect(200)
-        .expect(function(res) {
+        .expect(function (res) {
           if (typeof res.body === "string") {
             res.body = JSON.parse(res.body);
           }
@@ -161,7 +161,7 @@ describe("Articles Route:", function() {
     /**
      * Aquí pasamos un id incorrecto al URL, deberíamos tener un error 404
      */
-    xit("retorna un error 404 si el id no es correcto", function() {
+    it("retorna un error 404 si el id no es correcto", function () {
       return agent.get("/articles/76142896").expect(404);
     });
   });
@@ -170,7 +170,7 @@ describe("Articles Route:", function() {
    * Series de tests para testear la creación de nuevos Articles
    * usando un POST request a /articles
    */
-  describe("POST /articles", function() {
+  describe("POST /articles", function () {
     /**
      * Testea la creación de un articulo
      * Aquí nosotros no solo conseguimos devuelta el articulo,
@@ -181,15 +181,15 @@ describe("Articles Route:", function() {
      *  }
      *
      */
-    xit("creates a new article", function() {
+    it("creates a new article", function () {
       return agent
         .post("/articles")
         .send({
           title: "Awesome POST-Created Article",
-          content: "Can you believe I did this in a test?"
+          content: "Can you believe I did this in a test?",
         })
         .expect(201)
-        .expect(function(res) {
+        .expect(function (res) {
           expect(res.body.message).to.equal("Created successfully");
           expect(res.body.article.id).to.not.be.an("undefined");
           expect(res.body.article.title).to.equal(
@@ -200,30 +200,30 @@ describe("Articles Route:", function() {
 
     // Esta debería fallar con un 500 porque no seteamos el article.content
 
-    xit("no crea un nuevo articulo sin contenido", function() {
+    it("no crea un nuevo articulo sin contenido", function () {
       return agent
         .post("/articles")
         .send({
-          title: "Este articulo no debería ser permitido"
+          title: "Este articulo no debería ser permitido",
         })
         .expect(500);
     });
 
     // Chequeá si los articulos fueron realmente guardados a la base de datos
-    xit("guarda los articulos a la BD", function() {
+    it("guarda los articulos a la BD", function () {
       return agent
         .post("/articles")
         .send({
           title: "Awesome POST-Created Article",
-          content: "Can you believe I did this in a test?"
+          content: "Can you believe I did this in a test?",
         })
         .expect(201)
-        .then(function() {
+        .then(function () {
           return Article.findOne({
-            where: { title: "Awesome POST-Created Article" }
+            where: { title: "Awesome POST-Created Article" },
           });
         })
-        .then(function(foundArticle) {
+        .then(function (foundArticle) {
           expect(foundArticle).to.exist; // eslint-disable-line no-unused-expressions
           expect(foundArticle.content).to.equal(
             "Can you believe I did this in a test?"
@@ -233,17 +233,17 @@ describe("Articles Route:", function() {
 
     // No asumas que las operaciones async (como escrituras en la BD)
     // va a funcionar. Siempre chequeá
-    xit("Envía devuelta JSON del articulo creado, no solo la data POSTeada", function() {
+    it("Envía devuelta JSON del articulo creado, no solo la data POSTeada", function () {
       return agent
         .post("/articles")
         .send({
           title: "Coconuts",
           content: "A full-sized coconut weighs about 1.44 kg (3.2 lb).",
           extraneous:
-            "Sequelize va a ignorar silenciosamente esta propiedad que no es del esquema"
+            "Sequelize va a ignorar silenciosamente esta propiedad que no es del esquema",
         })
         .expect(201)
-        .expect(function(res) {
+        .expect(function (res) {
           expect(res.body.article.extraneous).to.be.an("undefined");
           expect(res.body.article.createdAt).to.exist; // eslint-disable-line no-unused-expressions
         });
@@ -254,14 +254,14 @@ describe("Articles Route:", function() {
    * Series de specs para testear actualizar los Articles usando pedido PUT
    *  a /articles/:id
    */
-  describe("PUT /articles/:id", function() {
+  describe("PUT /articles/:id", function () {
     let article;
 
-    beforeEach(function() {
+    beforeEach(function () {
       return Article.create({
         title: "Final Article",
-        content: "You can do it!"
-      }).then(function(createdArticle) {
+        content: "You can do it!",
+      }).then(function (createdArticle) {
         article = createdArticle;
       });
     });
@@ -276,14 +276,14 @@ describe("Articles Route:", function() {
      *  }
      *
      **/
-    xit("actualiza un article", function() {
+    it("actualiza un article", function () {
       return agent
         .put("/articles/" + article.id)
         .send({
-          title: "Awesome PUT-Updated Article"
+          title: "Awesome PUT-Updated Article",
         })
         .expect(200)
-        .expect(function(res) {
+        .expect(function (res) {
           expect(res.body.message).to.equal("Updated successfully");
           expect(res.body.article.id).to.not.be.an("undefined");
           expect(res.body.article.title).to.equal(
@@ -293,22 +293,22 @@ describe("Articles Route:", function() {
         });
     });
 
-    xit("guarda la actualización a la BD", function() {
+    it("guarda la actualización a la BD", function () {
       return agent
         .put("/articles/" + article.id)
         .send({
-          title: "Awesome PUT-Updated Article"
+          title: "Awesome PUT-Updated Article",
         })
-        .then(function() {
+        .then(function () {
           return Article.findByPk(article.id);
         })
-        .then(function(foundArticle) {
+        .then(function (foundArticle) {
           expect(foundArticle).to.exist; // eslint-disable-line no-unused-expressions
           expect(foundArticle.title).to.equal("Awesome PUT-Updated Article");
         });
     });
 
-    xit("Obtiene un 500 por un update invalido", function() {
+    it("Obtiene un 500 por un update invalido", function () {
       return agent
         .put("/articles/" + article.id)
         .send({ title: "" })
