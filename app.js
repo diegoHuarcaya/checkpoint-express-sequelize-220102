e3e97f93b33ac50892f78937b3656cc58842696a
Facu Velasco <velascofacundo@gmail.com>
2020-10-06 11:23:40 -0300

    feat: test & grade script

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var chalk = require('chalk');
-var path = require('path');
+var express = require("express");
+var morgan = require("morgan");
+var bodyParser = require("body-parser");
+var chalk = require("chalk");
+var path = require("path");
 
-var routes = require('./routes');
+var routes = require("./routes");
 
 var app = express();
 
-app.use(morgan('      ↓ received :method :url · responded :status :res[Content-Type]'));
+if (process.env.MODE !== "grade") {
+  app.use(
+    morgan(
+      "      ↓ received :method :url · responded :status :res[Content-Type]"
+    )
+  );
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function (req, res, next) {
-  res.send('Welcome to Articles! Check out our lovely routes.');
+app.get("/", function (req, res, next) {
+  res.send("Welcome to Articles! Check out our lovely routes.");
 });
 
-app.use('/', routes);
+app.use("/", routes);
 
 // custom error handling
 app.use(function (err, req, res, next) {
@@ -25,17 +32,17 @@ app.use(function (err, req, res, next) {
   if (!err.stack || !err.message) next(err);
   // clean up the trace to just relevant info
   var cleanTrace = err.stack
-  .split('\n')
-  .filter(line => {
-    // comment out the next two lines for full (verbose) stack traces
-    var projectFile = line.indexOf(__dirname) > -1; // omit built-in Node code
-    var nodeModule = line.indexOf('node_modules') > -1; // omit npm modules
-    return projectFile && !nodeModule;
-  })
-  .join('\n');
+    .split("\n")
+    .filter((line) => {
+      // comment out the next two lines for full (verbose) stack traces
+      var projectFile = line.indexOf(__dirname) > -1; // omit built-in Node code
+      var nodeModule = line.indexOf("node_modules") > -1; // omit npm modules
+      return projectFile && !nodeModule;
+    })
+    .join("\n");
   // colorize and format the output
-  console.log(chalk.magenta('      ' + err.message));
-  console.log('    ' + chalk.gray(cleanTrace));
+  console.log(chalk.magenta("      " + err.message));
+  console.log("    " + chalk.gray(cleanTrace));
   // send back error status
   res.status(err.status || 500).end();
 });
